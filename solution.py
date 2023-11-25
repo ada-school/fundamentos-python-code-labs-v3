a6a9155aea65164ac73160952a2b8539eff6a639
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-25 02:56:28 -0400

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,15 @@
+def agregar_producto(inventario, producto):
+    # Añadir o actualizar producto en el inventario
+    pass
+
+def actualizar_stock(inventario, nombre_producto, nueva_cantidad):
+    # Actualizar la cantidad de un producto
+    pass
+
+def calcular_valor_total(inventario):
+    # Calcular el valor total del inventario
+    pass
+
+def listar_productos(inventario):
+    # Listar todos los productos en el inventario
+    pass
