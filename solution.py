79ffd060291ed8eedabe9f6225051f9a954690ad
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-25 02:05:35 -0400

    Create solution.py lab 3

---
+++ solution.py
@@ -0,0 +1,11 @@
+def reservar_asiento(evento: list[list[bool]], fila: int, columna: int) -> str:
+    pass
+
+def cancelar_reserva(evento: list[list[bool]], fila: int, columna: int) -> str:
+    pass
+
+def mostrar_estado(evento: list[list[bool]]) -> None:
+    pass
+
+def verificar_completa_reserva(evento: list[list[bool]]) -> str:
+    pass
