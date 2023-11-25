9947e8ae5ff0e23fa0b251ab9a7217a6487e3486
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-25 03:20:45 -0400

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,17 @@
+eventos = {}
+
+def agregar_evento(nombre, fecha):
+    # Tu implementación aquí
+    pass
+
+def listar_eventos():
+    # Tu implementación aquí
+    pass
+
+def calcular_precio(edad):
+    # Tu implementación aquí
+    pass
+
+def buscar_evento(nombre):
+    # Tu implementación aquí
+    pass
