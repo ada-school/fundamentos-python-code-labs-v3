6174ec29482dda7ba07d5c8df6ff928150a43d0e
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-28 19:17:50 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
 
 
+class Cliente:
+    def __init__(self, nombre, id):
+        self.nombre = nombre
+        self.id = id
+
+
 class Reserva:
     def __init__(self, cliente, fecha_inicio, fecha_fin):
         self.cliente = cliente
