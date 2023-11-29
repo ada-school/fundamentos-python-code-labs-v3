fee79b4910264d8de2afbff851f7bc3afd4ca649
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-29 19:04:22 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,18 @@
-def agregar_producto(inventario, producto):
+from typing import Any
+
+
+def agregar_producto(inventario: dict[str, dict[str, Any]], producto: dict[str, Any]):
     # Añadir o actualizar producto en el inventario
     pass
 
-def actualizar_stock(inventario, nombre_producto, nueva_cantidad):
+def actualizar_stock(inventario: dict[str, dict[str, Any]], nombre_producto: str, nueva_cantidad: int):
     # Actualizar la cantidad de un producto
     pass
 
-def calcular_valor_total(inventario):
+def calcular_valor_total(inventario: dict[str, dict[str, Any]]):
     # Calcular el valor total del inventario
     pass
 
-def listar_productos(inventario):
+def listar_productos(inventario: dict[str, dict[str, Any]]):
     # Listar todos los productos en el inventario
     pass
