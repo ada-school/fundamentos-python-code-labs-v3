9e7acd10490a0db19dca889d8dab12fbc3574ec6
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-29 19:11:00 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,20 +1,21 @@
 class Vehiculo:
-    def __init__(self, marca, modelo, año, precio, estado):
+    def __init__(self, marca: str, modelo: str, año: int, precio: float, estado: str):
         self.marca = marca
         self.modelo = modelo
         self.año = año
         self.precio = precio
         self.estado = estado  # 'disponible' o 'vendido'
 
+
 class Concesionaria:
     def __init__(self):
         self.inventario = []
 
-    def agregar_vehiculo(self, vehiculo):
+    def agregar_vehiculo(self, vehiculo: Vehiculo):
         pass
 
-    def buscar_vehiculo(self, marca, modelo, año):
+    def buscar_vehiculo(self, marca: str, modelo: str, año: int):
         pass
 
-    def actualizar_estado_venta(self, vehiculo, nuevo_estado):
+    def actualizar_estado_venta(self, vehiculo: Vehiculo, nuevo_estado: str):
         pass
