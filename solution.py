0ab3fbcb57e665dacb44912a183b192d85a8df0d
Rafael Villca Poggian <rafael.rvp98@gmail.com>
2023-11-29 16:17:31 -0400

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,11 @@
+class Habitacion:
+    pass
+
+# Implementar las clases HabitacionEjecutiva y Suite aquí
+
+class Reserva:
+    pass
+
+    def calcular_total(self):
+        # Implementar el cálculo del costo total
+        pass
